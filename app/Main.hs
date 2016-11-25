dbdcb31129790b7e28e06e1a614b4697a08bf21e
Cono52 <oflanac52@gmail.com>
2016-11-25 23:31:43 +0000

    Tried connecting mongoDB, not building

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE OverloadedStrings    #-}
+
 module Main where
 
-import Lib
+import           Control.Monad.Trans (liftIO)
+import           Database.MongoDB    (Action, Document, Document, Value, access,
+                                      close, connect, delete, exclude, find,
+                                      host, insertMany, master, project, rest,
+                                      select, sort, (=:))
+
+import           Lib
 
 main :: IO ()
 main = startApp
+
+runMongo dbName functionToRun = do
+    pipe <- connect (host "127.0.0.1")
+    e <- access pipe master dbName functionToRun
+    close pipe
