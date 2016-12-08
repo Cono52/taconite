b06d54569604133dffd223fb16bdf68e7402eef0
Cono52 <oflanac52@gmail.com>
2016-12-08 21:55:18 +0000

    refactor

--- authService/src/Lib.hs
+++ authService/src/Lib.hs
@@ -14,33 +14,34 @@ module Lib
     ( startApp
     ) where
 
+import           Control.Monad.Trans      (liftIO)
 import           Data.Aeson
-import           Data.Proxy
 import           Data.Aeson.TH
+import qualified Data.ByteString.Char8    as C
+import           Data.Char
 import           Data.List                (sortBy)
 import           Data.Ord                 (comparing)
+import           Data.Proxy
 import           Data.String
-import           Data.Char
 import           Data.Time.Calendar
-import qualified Data.ByteString.Char8 as C
-import           System.Random
+import           Data.Typeable
+import           GHC.Generics
+import           Network.HTTP.Client      (defaultManagerSettings, newManager)
 import           Network.Wai
 import           Network.Wai.Handler.Warp
-import           Network.HTTP.Client (newManager, defaultManagerSettings)
 import           Servant
-import           Servant.Client
 import           Servant.API
-import           GHC.Generics
-import           Control.Monad.Trans      (liftIO)
+import           Servant.Client
+import           System.Random
 
 
 
 
 --Auth API
 
-data Token = Token 
+data Token = Token
   { token :: Int
-  } deriving (Show, Generic, FromJSON, ToJSON) 
+  } deriving (Show, Generic, FromJSON, ToJSON)
 
 
 startApp :: IO ()
@@ -63,26 +64,25 @@ server = login
 
 login :: Maybe String -> Handler Token
 login uname = liftIO $ do
-  e <- getUsers
-  print e
-  return $ Token(10)
+  u <- getUsers
+  return $ Token 10
 
 --encryption code
 data PublicKey = PublicKey { pubkey :: Int } deriving (Show, Generic, FromJSON, ToJSON)
 data PrivateKey = PrivateKey { prikey :: Int } deriving (Show, Generic, FromJSON, ToJSON)
 
 modulto = 256
-testPub = PublicKey(123)
-testPri = PrivateKey(133)
+testPub = PublicKey 123
+testPri = PrivateKey 133
 
 generateKeys :: IO Int
-generateKeys = randomRIO(1, modulto - 1) --traped in IO 
+generateKeys = randomRIO(1, modulto - 1) --traped in IO
 
 encrypt :: String -> PublicKey -> String
-encrypt str p =  map (\a -> chr $ ((ord a) + (pubkey p)) `mod` modulto) str
+encrypt str p =  map (\a -> chr $ (ord a + pubkey p) `mod` modulto) str
 
 decrypt :: String -> PrivateKey -> String
-decrypt str p =  map (\a -> chr $ ((ord a) + (prikey p)) `mod` modulto) str
+decrypt str p =  map (\a -> chr $ (ord a + prikey p) `mod` modulto) str
 
 getPublicKey :: PublicKey
 getPublicKey = testPub
@@ -94,7 +94,7 @@ data User = User
   } deriving (Show, Generic, FromJSON, ToJSON)
 
 
-data UserFile = UserFile 
+data UserFile = UserFile
   { file :: String
   } deriving (Show, Generic, FromJSON, ToJSON)
 
@@ -106,30 +106,22 @@ instance ToJSON ResponseData
 instance FromJSON ResponseData
 
 
-users :: Maybe String -> ClientM [User]
+getUserByName :: Maybe String -> ClientM [User]
 
 saveFile :: UserFile -> ClientM ResponseData
 
-type DataAPI = "users" :> QueryParam "search" String :> Get '[JSON] [User]
+type DataAPI = "getUserByName" :> QueryParam "search" String :> Get '[JSON] [User]
           :<|> "saveFile" :> ReqBody '[JSON] UserFile :> Post '[JSON] ResponseData
 
 dataAPI :: Proxy DataAPI
 dataAPI = Proxy
 
-
-(users :<|> saveFile) = client dataAPI
-
-
-queries :: ClientM [User]
-queries = do
-  u <- users (Just "")
-  return u
+(getUserByName :<|> saveFile) = client dataAPI
 
 getUsers :: IO ()
 getUsers = do
   manager <- newManager defaultManagerSettings
-  res <- runClientM queries (ClientEnv manager (BaseUrl Http "127.0.0.1" 8000 ""))
+  res <- runClientM (getUserByName (Just "conor")) (ClientEnv manager (BaseUrl Http "127.0.0.1" 8000 ""))
   case res of
-    Left err -> putStrLn $ "Error: " ++ show err
-    Right (users) -> do
-      print users
+    Left err   -> putStrLn $ "Error: " ++ show err
+    Right usrs -> print usrs
