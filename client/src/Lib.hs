110f7385e36ab03557e0f80effe32496bf7e36d0
Cono52 <oflanac52@gmail.com>
2016-12-15 20:36:39 +0000

    added libs to client

--- client/src/Lib.hs
+++ client/src/Lib.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE DeriveAnyClass       #-}
+{-# LANGUAGE DeriveGeneric        #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE OverloadedStrings    #-}
 {-# LANGUAGE StandaloneDeriving   #-}
 {-# LANGUAGE TemplateHaskell      #-}
 {-# LANGUAGE TypeOperators        #-}
-{-# LANGUAGE DeriveGeneric        #-}
-{-# LANGUAGE DeriveAnyClass       #-}
-{-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 
 
@@ -14,33 +14,42 @@ module Lib
     ( startApp
     ) where
 
+
+import           Control.Monad            (forever, when)
+import           Control.Monad.Trans      (liftIO)
 import           Data.Aeson
 import           Data.Aeson.TH
+import qualified Data.ByteString.Char8    as C
+import           Data.Char
 import           Data.List                (sortBy)
 import           Data.Ord                 (comparing)
+import           Data.Proxy
 import           Data.String
-import           Data.Char
 import           Data.Time.Calendar
+import           Data.Typeable
+import           GHC.Generics
+import           Network.HTTP.Client      (defaultManagerSettings, newManager)
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           Servant
-import           System.Exit             
-import           GHC.Generics
-import           Control.Monad.Trans      (liftIO)
-import           Control.Monad            (forever, when)
+import           Servant.API
+import           Servant.Client
+import           System.Exit
+import           System.Random
+
 
 
 startApp :: IO ()
-startApp = forever $ do 
-    l <- getLine  
+startApp = forever $ do
+    l <- getLine
     when (l == "quit") $ do
             putStrLn ""
             putStrLn "goodbye..."
             exitSuccess
-    when (l == "saveFile") $ do  
+    when (l == "saveFile") $ do
             putStrLn "saved!\n"
     when (l == "readFile") $ do
-            putStrLn "" 
+            putStrLn ""
             putStrLn "here you go..."
     when (l == "login") $ do
             putStrLn ""
